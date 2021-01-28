006eabd8145866126fe80b5a71e79520a01d1072
namjin.kim <namjin.kim@deliveryhero.co.kr>
2021-01-28 15:12:45 +0900

    test upload

---
+++ tmp.py
@@ -0,0 +1,11 @@
+SECRET_KEY = "h^jz=pr_u0c&%basdfsdf_s&1h!w+mf*i$mm*ithp"
+-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAty2QP1wibWXkpROE6dj4wQfGtCHrBTK/Scha0A8cM6IgisE8
+1fdcmFz2f4wU/tlJ9EM80FH2gNs2OsdPXuCspSadO/UCz7iTJKiO8lNdrRvrWNRg
+ahH1S/PHFhrHH35r1a7bRjf9ho3tKEUeIJcweEcn5COiKpKeN1a9v0oIBwxRZ3Yh6
+Fe/1AoGAT1xQhuYPA04E4YDmQDsas4SsZiJ5LG2pK8O/1VIzkeZz0ZpRgTNQ/V23
+GGhAscxx+FchaUpFnFVS5VbGPlvKF6CAf2DI+4qeS8mzOruiYUB2sAxnrr53k/xw
+asdfalkdfjakdlfjakdsjflksadjflkasjdflksjdfkf;sfjdflsfsddflafjfkf
+7vl0983eZlIoLl7zq/sgQcsVRziiKB3ujqfuEhOAd8+mIOdoFhk=
+-----END RSA PRIVATE KEY-----
+client_id=AROA254JasdfsadfLRZTSMGJHMXO\nclient_secret=lGm6b6asdfsadfsdoZ9DR3qObOlt5
